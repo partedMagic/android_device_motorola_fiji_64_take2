e7c93372698338de19693ffba32a630c948a8ad9
PartedMagic <74645668+partedMagic@users.noreply.github.com>
2023-04-15 16:46:39 +0100

    Update omni_fiji_64.mk
    PBRP Fix

--- omni_fiji_64.mk
+++ omni_fiji_64.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Omni stuff. |  PBRP Fix
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from fiji_64 device
 $(call inherit-product, device/motorola/fiji_64/device.mk)
